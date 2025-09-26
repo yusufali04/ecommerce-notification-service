52f6c7ac003b9aaf23d278a7157523be31ecd3b1
Yusuf <yusufali.5094@gmail.com>
2025-09-26 22:28:40 +0530

    Initial commit

---
+++ server.ts
@@ -0,0 +1,20 @@
+import logger from "./src/config/logger";
+import { createMessageBroker } from "./src/factories/broker-factory";
+import { MessageBroker } from "./src/types/broker";
+
+const startServer = async () => {
+  let broker: MessageBroker | null = null;
+  try {
+    broker = createMessageBroker();
+    await broker.connectConsumer();
+    await broker.consumeMessage(["order"], false);
+  } catch (err) {
+    logger.error("Error happened: ", err.message);
+    if (broker) {
+      await broker.disconnectConsumer();
+    }
+    process.exit(1);
+  }
+};
+
+void startServer();
